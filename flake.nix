ed9125ddc5619f899c310eda639fa89e9dacbd33
Johannes Jeising <johannes.jeising@gmail.com>
2024-06-24 20:31:18 +0200

    introduce sphinx needs overlay

--- flake.nix
+++ flake.nix
@@ -6,13 +6,16 @@
     # nixpkgs-23-11.url = "github:nixos/nixpkgs?ref=nixos-23.11";
     nixpkgs.url = github:nixos/nixpkgs?ref=nixos-unstable;
     flake-utils.url = "github:numtide/flake-utils";
+    nix-needs.url = "github:jsqu4re/nix-needs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-needs, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs { 
+            overlays = [ nix-needs.overlays.sphinx-needs ]; 
+          };
         in
         {
           devShells.default = import ./shell.nix { inherit pkgs; };
--- shell.nix
+++ shell.nix
@@ -3,7 +3,16 @@ let
 sphinx-needs-pkg = import ./nix/sphinx-needs-pkgs.nix;
 in 
   pkgs.mkShell {
-    packages = (sphinx-needs-pkg{inherit pkgs;}).pkgs;
+    packages = [
+      sphinx-needs
+      sphinxcontrib-test-reports
+      sphinx-simplepdf
+      sphinx-preview
+      sphinx-immaterial
+      matplotlib
+      sphinx-copybutton
+      sphinxcontrib-programoutput
+    ];
 
     shellHook = ''
       # ...
